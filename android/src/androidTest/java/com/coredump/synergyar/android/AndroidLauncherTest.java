682ac1649d5baa97054e58870ecd4c682aae8ab1
Esteban <joseestetq@hotmail.com>
2015-10-31 07:32:34 -0600

    Integrate testing module

---
+++ android/src/androidTest/java/com/coredump/synergyar/android/AndroidLauncherTest.java
@@ -0,0 +1,54 @@
+package com.coredump.synergyar.android;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.SmallTest;
+
+/**
+ * Checks correctness of the instantiation of the app.
+ *
+ * @author Jose Esteban Trejos Quiros
+ * @version 0.0.1
+ * @since 0.0.1
+ */
+public class AndroidLauncherTest extends ActivityInstrumentationTestCase2<AndroidLauncher> {
+
+  /**
+  * No parameter constructor.
+  */
+  public AndroidLauncherTest() {
+    super(AndroidLauncher.class);
+  }
+
+  /**
+  * Prepares the test.
+  */
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+  }
+
+  /**
+  * Frees resources.
+  */
+  @Override
+  protected void tearDown() throws Exception {
+    super.tearDown();
+  }
+
+  /**
+  * Runs a dummie test.
+  */
+  @Override
+  protected void runTest() throws Throwable {
+    super.runTest();
+  }
+
+  /**
+  * Fires the test.
+  */
+  @SmallTest
+  public void testDoSomething() throws Throwable {
+    assertEquals("", "");
+  }
+
+}
