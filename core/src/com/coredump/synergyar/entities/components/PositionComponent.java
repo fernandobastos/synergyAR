030acf4b5bc0545a1f78ca43888695190847baf1
prismocai <franm29@hotmail.com>
2015-12-09 21:30:45 -0600

    Distancia entre 2 objetos espacio 3D  y 2D

---
+++ core/src/com/coredump/synergyar/entities/components/PositionComponent.java
@@ -0,0 +1,11 @@
+package com.coredump.synergyar.entities.components;
+
+import com.badlogic.ashley.core.Component;
+
+/**
+ * Created by Franz on 17/11/2015.
+ */
+public class PositionComponent implements Component {
+
+
+}
