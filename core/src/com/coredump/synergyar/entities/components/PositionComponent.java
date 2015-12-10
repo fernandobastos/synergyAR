4340abcf84c94b8fcf192bd96b0fe2ff1318b58f
prismocai <franm29@hotmail.com>
2015-12-09 21:39:17 -0600

    Vector3 in PositionComponent

--- core/src/com/coredump/synergyar/entities/components/PositionComponent.java
+++ core/src/com/coredump/synergyar/entities/components/PositionComponent.java
@@ -1,11 +1,12 @@
 package com.coredump.synergyar.entities.components;
 
 import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.math.Vector3;
 
 /**
  * Created by Franz on 17/11/2015.
  */
 public class PositionComponent implements Component {
 
-
+    public Vector3 position;
 }
